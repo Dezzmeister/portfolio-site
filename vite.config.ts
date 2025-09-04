3d2676a50bf0403003b96023edec63f3a3e87704
Joe Desmond <joe.github@obama69.com>
2025-09-04 00:23:40 -0400

    Build a multi-page site correctly

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,17 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import svgr from "vite-plugin-svgr";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact(), svgr()]
+  plugins: [preact(), svgr()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        attributions: resolve(__dirname, "attributions.html")
+      }
+    }
+  }
 })
